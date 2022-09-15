e4dd4b280dacc2bc6201e2e05aa7068a71fa6c37
Dale Walter <dalewaltergh@gmail.com>
2022-09-16 04:50:34 +0800

    searching restaurants

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import { ThemeProvider } from "styled-components/native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Ionicons } from "@expo/vector-icons";
 
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
-import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import { 
+  useFonts as useLato, 
+  Lato_400Regular 
+} from "@expo-google-fonts/lato";
 
 import { theme } from "./src/infrastructure/theme";
-import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
-import { SafeArea } from "./src/components/utility/safe-area.component";
-import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
+import { SafeArea } from "./src/components/utility/safe-area.component"; 
 import { LocationContextProvider } from "./src/services/location/location.context";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 
 const Tab = createBottomTabNavigator();
 
@@ -43,6 +46,7 @@ const createScreenOptions = ({ route }) => {
     tabBarIcon: ({ size, color }) => (
       <Ionicons name={iconName} size={size} color={color} />
     ),
+    headerShown: false
   };
 };
 
