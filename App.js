3650eb97341fcceb1dc0824121509fc2e12576f4
Walter <dalewaltergh@gmail.com>
2022-09-19 01:10:55 +0800

    firebase installation

--- App.js
+++ App.js
@@ -10,6 +10,19 @@ import { LocationContextProvider } from "./src/services/location/location.contex
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZxwdV_HyXswJ2y4jApq8MgCdVF0ty-9E",
+  authDomain: "mealstogo-e2e6a.firebaseapp.com",
+  projectId: "mealstogo-e2e6a",
+  storageBucket: "mealstogo-e2e6a.appspot.com",
+  messagingSenderId: "947013286871",
+  appId: "1:947013286871:web:a0c59228d52f1fe4bc2992"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 export default function App() {
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
