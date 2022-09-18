92ceb52b58098947bde05e620b0cdfa4eb362792
Walter <dalewaltergh@gmail.com>
2022-09-19 00:46:19 +0800

    storing favourites

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,9 +1,30 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = ({ children }) => {
   const [favourites, setFavourites] = useState([]);
+
+  const saveFavourites = async value => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem("@favourites", jsonValue);
+    } catch (error) {
+      console.log("Error Saving:", error);
+    }
+  };
+
+  const loadFavourites = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@favourites");
+      if (value !== null) {
+        setFavourites(JSON.parse(value));
+      }
+    } catch (error) {
+      console.log("Error Loading:", error);
+    }
+  };
 
   const add = restaurant => {
     setFavourites([...favourites, restaurant]);
@@ -13,6 +34,14 @@ export const FavouritesContextProvider = ({ children }) => {
     const newFavourites = favourites.filter(x => x.placeId !== restaurant.placeId);
     setFavourites(newFavourites);
   };
+
+  useEffect(() => {
+    loadFavourites(favourites);
+  }, []);
+
+  useEffect(() => {
+    saveFavourites(favourites);
+  }, [favourites]);
 
   return (
     <FavouritesContext.Provider value={{
