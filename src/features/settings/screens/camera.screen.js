78729a61c590cc5f27e9fa24f8471d3fb89c9123
Dale Walter <dalewaltergh@gmail.com>
2022-09-20 15:25:35 +0800

    storing and retrieving picture

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,17 +1,31 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useContext } from "react";
 import styled from "styled-components/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import { View } from "react-native";
 import { Camera, CameraType } from "expo-camera";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import { Text } from "../../../components/typography/text.component";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
 const ProfileCamera = styled(Camera)`
   width: 100%;
   height: 100%;
 `;
 
-export const CameraScreen = () => {
-  const [permission, requestPermission] = Camera.useCameraPermissions();
+export const CameraScreen = ({ navigation }) => {
   const cameraRef = useRef();
+  const { user } = useContext(AuthenticationContext);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  const snap = async () => {
+    console.log("Snap!")
+    if (cameraRef) {
+      const photo = await cameraRef.current.takePictureAsync();
+      AsyncStorage.setItem(`${user.uid}-photo`, photo.uri);
+      navigation.goBack();
+    }
+  };
 
   if (!permission) {
     return <View />
@@ -20,7 +34,8 @@ export const CameraScreen = () => {
   }
 
   return (
-    <ProfileCamera ref={cameraRef} type={CameraType.front}>
-    </ProfileCamera>
+    <TouchableOpacity onPress={snap}>
+      <ProfileCamera ref={cameraRef} type={CameraType.front} />
+    </TouchableOpacity>
   );
 };
