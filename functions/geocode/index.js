20570efdfb17b5e26339410172bc43772c0d726f
Dale Walter <dalewaltergh@gmail.com>
2022-09-21 02:15:49 +0800

    setup google node sdk

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,8 +1,22 @@
 const { locations: locationsMock } = require("./geocode.mock");
 const url = require("url");
 
-module.exports.geocodeRequest = (req, res) => {
-  const { city }= url.parse(req.url, true).query;
-  const locationMock = locationsMock[city?.toLowerCase()];
-  res.json(locationMock);
+module.exports.geocodeRequest = (request, response, client) => {
+  const { city, mock }= url.parse(request.url, true).query;
+  if (mock === "true") {
+    const locationMock = locationsMock[city?.toLowerCase()];
+    return response.json(locationMock);
+  }
+
+  client.geocode({
+    params: {
+      address: city,
+      key: "api key for google",
+    },
+    timeout: 1000,
+  }).then(res => response.json(res.data))
+  .catch(e => {
+    response.status(400);
+    return response.send(e.response.data.error_message);
+  })
 };
--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,13 @@ const functions = require("firebase-functions");
 const { geocodeRequest } = require("./geocode");
 const { placesRequest } = require("./places");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+const { Client } = require("@googlemaps/google-maps-services-js");
+const client = new Client({});
 
 exports.geocode = functions.https.onRequest((req, res) => {
-  geocodeRequest(req, res);
+  geocodeRequest(req, res, client);
 });
 
 exports.placesNearby = functions.https.onRequest((req, res) => {
-  placesRequest(req, res);
+  placesRequest(req, res, client);
 });
