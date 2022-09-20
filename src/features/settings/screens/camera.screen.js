d05cf930fea2b1a035d86ec89a92fe618d6b0fa5
Dale Walter <dalewaltergh@gmail.com>
2022-09-20 12:19:24 +0800

    expo camera

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,3 +1,26 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
+import styled from "styled-components/native";
+import { View } from "react-native";
+import { Camera, CameraType } from "expo-camera";
+import { Text } from "../../../components/typography/text.component";
 
-export const CameraScreen = () => null;
+const ProfileCamera = styled(Camera)`
+  width: 100%;
+  height: 100%;
+`;
+
+export const CameraScreen = () => {
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const cameraRef = useRef();
+
+  if (!permission) {
+    return <View />
+  } else if (!permission.granted){
+    return <Text>No access to camera</Text>
+  }
+
+  return (
+    <ProfileCamera ref={cameraRef} type={CameraType.front}>
+    </ProfileCamera>
+  );
+};
